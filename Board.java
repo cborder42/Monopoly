ea3bdaa6256e1806a39507cb88ab72dca525160b
rmehta1234 <77749430+rmehta1234@users.noreply.github.com>
2022-05-25 11:12:28 -0700

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,66 @@
+public class Board {
+    private Property[] board; 
+    public Board(){
+        board = new Property[] {
+            new Property("Go", false, false), 
+            new Property("Mediterranean Avenue", 60, 30, 50, new int[] {2, 10, 30, 90, 160, 250}, false, false), 
+            new Property("Community Chest", false, true),
+            new Property("Baltic Avenue", 60, 30, 50, new int[] {4, 20, 60, 180, 320, 450}, false, false),
+            new Property("Tax", false, false),
+            new Property("Reading Railroad", 200, false, false),
+            new Property("Oriental Avenue", 100, 50, 50, new int[] {6, 30, 90, 270, 400, 550}, false, false),
+            new Property("Chance", true, false),
+            new Property("Vermont Avenue", 100, 50, 50, new int[] {6, 30, 90, 270, 400, 550}, false, false),
+            new Property("Connecticut Avenue", 120, 60, 50, new int[] {8, 40, 100, 300, 450, 600}, false, false),
+            new Property("Jail", false, false),
+            new Property("St. Charles Place", 140, 70, 100, new int[] {10, 50, 150, 450, 625, 750}, false, false),
+            new Property("Electric Company", 150, false, false, true),
+            new Property("States Avenue", 140, 70, 100, new int[] {10, 50, 150, 450, 625, 750}, false, false),
+            new Property("Virginia Avenue", 160, 80, 100, new int[] {12, 60, 180, 500, 700, 900}, false, false),
+            new Property("Pennsylvania Railroad", 200, false, false),
+            new Property("St. James Place", 180, 90, 100, new int[] {14, 70, 200, 550, 750, 950}, false, false),
+            new Property("Community Chest", false, true),
+            new Property("Tennessee Avenue", 180, 90, 100, new int[] {14, 70, 200, 550, 750, 950}, false, false),
+            new Property("New York Avenue", 200, 100, 100, new int[] {16, 80, 220, 600, 800, 1000}, false, false),
+            new Property("Free Parking", false, false),
+            new Property("Kentucky Avenue", 220, 110, 150, new int[] {18, 90, 250, 700, 875, 1050}, false, false),
+            new Property("Chance", true, false),
+            new Property("Indiana Avenue", 220, 110, 150, new int[] {18, 90, 250, 700, 875, 1050}, false, false),
+            new Property("Illinois Avenue", 240, 120, 150, new int[] {20, 100, 300, 750, 925, 1100}, false, false),
+            new Property("B. & O. Railroad", 200, false, false),
+            new Property("Atlantic Avenue", 260, 130, 150, new int[] {22, 110, 330, 800, 975, 1150}, false, false),
+            new Property("Ventnor Avenue", 260, 130, 150, new int[] {22, 110, 330, 800, 975, 1150}, false, false),
+            new Property("Water Works", 150, false, false, true),
+            new Property("Marvin Gardens", 280, 140, 150, new int[] {24, 120, 360, 850, 1025, 1200}, false, false),
+            new Property("Go To Jail", false, false),
+            new Property("Pacific Avenue", 300, 150, 200, new int[] {26, 130, 390, 900, 1100, 1275}, false, false),
+            new Property("North Carolina Avenue", 300, 150, 200, new int[] {26, 130, 390, 900, 1100, 1275}, false, false),
+            new Property("Community Chest", false, true),
+            new Property("Pennsylvania Avenue", 320, 160, 200, new int[] {28, 150, 450, 1000, 1200, 1400}, false, false),
+            new Property("Short Line", 200, false, false),
+            new Property("Chance", true, false),
+            new Property("Park Place", 350, 175, 200, new int[] {35, 175, 500, 1100, 1300, 1500}, false, false),
+            new Property("Tax", false, false),
+            new Property("Boardwalk", 400, 200, 200, new int[] {50, 200, 600, 1400, 1700, 2000}, false, false),
+            
+
+
+
+
+        };
+    }
+    public Property getProperty(int position){
+        return board[position % board.length];
+    }
+    public int getTotalSpaces() {
+        return board.length;
+    }
+    public Boolean didPassGo(int oldPos, int newPos) {
+        if ((newPos % board.length) < (oldPos % board.length)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
