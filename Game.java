afa84d2c0353f0732345e3066c782177e1f453e5
Kaustav Mitra <88690776+KaustavMitra06@users.noreply.github.com>
2022-05-25 12:25:49 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,33 +2,66 @@ import java.util.*;
 public class Game{
     private static boolean gameContinue; 
     private ArrayList<Player> listOfPlayers = new ArrayList<Player>(); 
+    private ArrayList<Player> originalListOfPlayers = new ArrayList<Player>();
     Scanner in = new Scanner(System.in); 
 
     public static void main (String[] args){
         gameContinue = true;
+        Game g1 = new Game();
+        g1.simulation();
     }
 
     public Game(){
-        System.out.println ("How many players are playing?");
-        int numOfPlayers = in.nextInt();
-        for (int i = 0; i < numOfPlayers; i++){
-            listOfPlayers.add(new Player()); 
-        }
+        getPlayers();
     }
-
-    public void simulation(){
-        for (int i = 0; i < listOfPlayers.size(); i++){
-            Player currentPlayer = listOfPlayers.get(i);
-            currentPlayer.changePos((int)(Math.random()*12)+2);
-            if (board[currentPlayer.getPos()].owned() && board[currentPlayer.getPos()].isProperty()){
-                if(currentPlayer.getCanPay(board[currentPlayer.getPos()].cost()))
-                currentPlayer.subBal(board.getPayment());
-            }
-            if (currentPlayer.getBal()==0){
-                listOfPlayers.remove(i);
+    
+    public void getPlayers(){
+        System.out.println("How many players are playing? (Maximum 4)");
+        int numOfPlayers= in.nextInt();
+        if (numOfPlayers > 4){
+            System.out.println("Too many players");
+            getPlayers();
+        }
+        else{
+            for (int i = 0; i < numOfPlayers; i++){
+                listOfPlayers.add(new Player());
+                originalListOfPlayers.add(new Player());
             }
         }
+    }            
         
+    public void simulation(){
+        //checks if we should continue the game
+            for (int i = 0; i < listOfPlayers.size(); i++){
+                if (gameContinue){
+                    Player currentPlayer = listOfPlayers.get(i);
+            
+                    //one turn, going to implement all possibilities of the spot they are in here
+
+                    //calls changePos in the Player class with a given dice. 
+                    currentPlayer.changePos((int)(Math.random()*12)+2);
+
+                    //if the player lands on a position with a property on it
+
+                    if (board[currentPlayer.getPos()].owned() && board[currentPlayer.getPos()].isProperty()){
+                        if(currentPlayer.getCanPay(board[currentPlayer.getPos()].cost()))
+                        currentPlayer.subBal(board.getPayment());
+                    }
+
+                    //if the player does not have any money, they will be eliminated from the array of players. 
+                    if (currentPlayer.getBal()==0){
+                        listOfPlayers.remove(i);
+                    }
+
+                    //if there is only one player left, announce that the game is over
+                    if (listOfPLayers.size() == 1){
+                        gameContinue = false;
+                    }
+                }
+                else if (!gameContinue){
+                    getWinner(); 
+                }
+        }
     }
     public int pay(){
         if (!this.own()) {
@@ -44,4 +77,7 @@ public class Game{
             }
         }
     } 
+    public void getWinner(int i){
+        System.out.println(originalListOfPlayers.get(i).toString(i) + "has won.");
+    }
 }
