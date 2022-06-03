c50504ecfdcf71d55f1240f0077d896e0c44bcfc
rmehta1234 <77749430+rmehta1234@users.noreply.github.com>
2022-06-02 21:40:19 -0700

    Update Chance.java
    Using base class for chance and community objects

--- Chance.java
+++ Chance.java
@@ -1,40 +1,11 @@
-//Anya 
-//5/24/2022
+public class Chance extends ChanceCommunity{
 
-public class Chance{
-   private String name;
-   private boolean goJail;
-   private String whereGo;
-   private int money;
-   private boolean advance;
-   private boolean recieve;
-   private boolean community;
-
-  public Chance(String n, boolean jail, int mon, boolean adv, boolean recieveM, String gowhere, boolean isCommunity){
-     name = n;
-     goJail = jail;
-     money = mon;
-     advance = adv;
-     whereGo = gowhere;
-     community = isCommunity;
-     recieve = recieveM;
-     
-  
-  }
-  
-  public int getMoney(){
-     return money;
-  }
-  public boolean getRecieve(){
-     return recieve;
-  }
-  public String toString(){
-     return "Name: " + name + "Jail involved?: " + goJail + "How much money involved: " + money + "Advance?: " + advance + "Recieve the money?: " + recieve + "Where do you go?: " + whereGo + "Is it a community card: " + community;
-  
-  }
-  
-
-
-
-
-}
+ 
+   public Chance(String n, int mon, boolean adv, int gowhere){
+      super(n, mon, adv, gowhere);
+   }
+   
+   public String toString(){
+      return "Chance card:" + super.getName();
+   } 
+ }
