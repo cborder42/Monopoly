5bbcc369d2f5d388ee884709e09efc40c4467a5c
rmehta1234 <77749430+rmehta1234@users.noreply.github.com>
2022-06-03 18:13:20 -0700

    Update ChanceCards.java

--- ChanceCards.java
+++ ChanceCards.java
@@ -1,51 +1,26 @@
 public class ChanceCards{
-    private Chance[][] cards;
-    private int[] indexes = new int[] {0, 0};
-    public ChanceCards(){
-        cards = new Chance[][] {
-            new Chance[] {
-                new Chance("Advance to Boardwalk",0, "Boardwalk"),
-                new Chance("Advance to Go", 200, "Go"),
-                new Chance("Speeding fine", -15, null),
-                new Chance("Advance to the nearest Railroad", 0, "Railroad"),
-                new Chance("Go to Jail", -50, "Jail"),
-                new Chance("Advance token to nearest Utility", 0, "Utility"),
-                new Chance("Building loan matures", 150, null),
-                new Chance("Bank pays you dividend", 50, null)
-            },
-            new Chance[] {
-                new Chance("Advance to Go", 200, "Go"),
-                new Chance("Doctor's Fee", -50, null),
-                new Chance("You inherit $100", 100, null),
-                new Chance("Pay school fees", -50, null),
-                new Chance("Bank error in your favor", 200, null),
-                new Chance("Pay hospital fee", -50, null),
-                new Chance("You are assessed for street repair", -100, null),
-                new Chance("You won a second prize in a beauty contest", 10, null)
-            }
-        };
 
-       // Shuffle the cards.
-       for (Chance[] cardset : cards) {
-           shuffle(cardset);
-       }
-    }
+    private Chance[] cards; 
+     public ChanceCards(){
+       cards = new Chance[] {
+           new Chance("Advance to Boardwalk", 0, true, 39), //String name, boolean jail, int mon, boolean adv, String gowhere
+           new Chance("Advance to Go", 0, true, 0),
+           new Chance("Speeding fine. Lose $15.", -15, false, -1),
+            //new Chance("Advance to the nearest Railroad", 0, true, "Go"),
+           new Chance("Go to Jail", 0, true, 30),
+            //new Chance("Advance to nearest Utility", 0, true, "Utility"),
+           new Chance("Building loan matures. Get $150", 150, false, -1),
+           new Chance("Bank pays you dividend. Get $50.", 50, false, -1)
+       };
+   }
 
-    public Chance advanceCard(boolean isChance){
-        int type = isChance ? 0 : 1;
-        Chance[] cardset = cards[type];
-        indexes[type]++;
-        indexes[type] %= cardset.length;
-        return cardset[indexes[type]];
-    }
+   public Chance getChance() {
+       int randomCardNumber = (int) (Math.random() * cards.length);
+       return cards[randomCardNumber];
+   }
 
-    private void shuffle(Chance[] cards){
-        for (int i=0; i < 10; i++) {
-            int a = (int)(Math.random() * cards.length);
-            int b = (int)(Math.random() * cards.length);
-            Chance temp = cards[a];
-            cards[a] = cards[b];
-            cards[b] = temp;
-        }
-     }
+
+
+
+
 }
